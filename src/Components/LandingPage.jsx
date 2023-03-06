3f408beba7733c2735eb311599563581d5431b57
killer snake <123300427+PallavJain01@users.noreply.github.com>
2023-03-06 18:15:46 +0530

    Update LandingPage.jsx

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -6,29 +6,29 @@ const LandingPage = () => {
     <>
       <Typography variant="h4"
         sx={{
-          'color': colors.grey[50],
-          'fontWeight': 500,
-          'marginY': 20
+          color: colors.grey[50],
+          fontWeight: 500,
+          marginY: 20
         }}
         className="fitWidth center">
         Startup 3</Typography>
 
       <Typography variant="h1"
         sx={{
-          'color': colors.grey[50],
-          'fontWeight': 500,
-          'marginY': -15
+          color: colors.grey[50],
+          fontWeight: 500,
+          marginY: -15
         }}
         className="fitWidth center">
         Forget About Code</Typography>
 
       <Typography variant="h5"
         sx={{
-          'color': colors.grey[50],
-          'fontWeight': 400,
-          'width': '40vw',
-          'textAlign': 'center',
-          'marginY': 25
+          color: colors.grey[50],
+          fontWeight: 400,
+          width: '40vw',
+          textAlign: 'center',
+          marginY: 25
         }}
         className="center">
         Startup Framework gives you complete freedom over your creative process — you don’t have to think about any technical aspects. There are no limits and absolutely no coding.</Typography>
@@ -37,15 +37,15 @@ const LandingPage = () => {
         variant='contained'
         className='center'
         sx={{
-          'backgroundColor': '#482BE7',
-          'borderRadius': '25px',
-          'position': 'absolute',
-          'paddingY': 1.75,
-          'paddingX': 5,
-          'marginY': -15,
+          backgroundColor: '#482BE7',
+          borderRadius: '25px',
+          position: 'absolute',
+          paddingY: 1.75,
+          paddingX: 5,
+          marginY: -15,
           ":hover":
           {
-            'backgroundColor': '#482BF7'
+            backgroundColor: '#482BF7'
           }
 
         }}
